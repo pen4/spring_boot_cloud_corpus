76f5f6f271df10cca570628371ae0c61c0d7a527
kangxd <kangxd@devops.com>
2023-06-26 17:30:27 +0800

    grpc method protos case

---
+++ grpc-exe-demo/src/main/java/com/springboot/rpc/nameresovler/HelloServiceGrpc.java
@@ -0,0 +1,280 @@
+package com.springboot.rpc.nameresovler;
+
+import static io.grpc.MethodDescriptor.generateFullMethodName;
+import static io.grpc.stub.ClientCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncUnaryCall;
+import static io.grpc.stub.ClientCalls.blockingServerStreamingCall;
+import static io.grpc.stub.ClientCalls.blockingUnaryCall;
+import static io.grpc.stub.ClientCalls.futureUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
+
+/**
+ */
+@javax.annotation.Generated(
+    value = "by gRPC proto compiler (version 1.16.1)",
+    comments = "Source: helloworld.proto")
+public final class HelloServiceGrpc {
+
+  private HelloServiceGrpc() {}
+
+  public static final String SERVICE_NAME = "com.springboot.rpc.nameresovler.HelloService";
+
+  // Static method descriptors that strictly reflect the proto.
+  private static volatile io.grpc.MethodDescriptor<com.springboot.rpc.nameresovler.HelloMessage,
+      com.springboot.rpc.nameresovler.HelloResponse> getSayHelloMethod;
+
+  @io.grpc.stub.annotations.RpcMethod(
+      fullMethodName = SERVICE_NAME + '/' + "SayHello",
+      requestType = com.springboot.rpc.nameresovler.HelloMessage.class,
+      responseType = com.springboot.rpc.nameresovler.HelloResponse.class,
+      methodType = io.grpc.MethodDescriptor.MethodType.UNARY)
+  public static io.grpc.MethodDescriptor<com.springboot.rpc.nameresovler.HelloMessage,
+      com.springboot.rpc.nameresovler.HelloResponse> getSayHelloMethod() {
+    io.grpc.MethodDescriptor<com.springboot.rpc.nameresovler.HelloMessage, com.springboot.rpc.nameresovler.HelloResponse> getSayHelloMethod;
+    if ((getSayHelloMethod = HelloServiceGrpc.getSayHelloMethod) == null) {
+      synchronized (HelloServiceGrpc.class) {
+        if ((getSayHelloMethod = HelloServiceGrpc.getSayHelloMethod) == null) {
+          HelloServiceGrpc.getSayHelloMethod = getSayHelloMethod = 
+              io.grpc.MethodDescriptor.<com.springboot.rpc.nameresovler.HelloMessage, com.springboot.rpc.nameresovler.HelloResponse>newBuilder()
+              .setType(io.grpc.MethodDescriptor.MethodType.UNARY)
+              .setFullMethodName(generateFullMethodName(
+                  "com.springboot.rpc.nameresovler.HelloService", "SayHello"))
+              .setSampledToLocalTracing(true)
+              .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  com.springboot.rpc.nameresovler.HelloMessage.getDefaultInstance()))
+              .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  com.springboot.rpc.nameresovler.HelloResponse.getDefaultInstance()))
+                  .setSchemaDescriptor(new HelloServiceMethodDescriptorSupplier("SayHello"))
+                  .build();
+          }
+        }
+     }
+     return getSayHelloMethod;
+  }
+
+  /**
+   * Creates a new async stub that supports all call types for the service
+   */
+  public static HelloServiceStub newStub(io.grpc.Channel channel) {
+    return new HelloServiceStub(channel);
+  }
+
+  /**
+   * Creates a new blocking-style stub that supports unary and streaming output calls on the service
+   */
+  public static HelloServiceBlockingStub newBlockingStub(
+      io.grpc.Channel channel) {
+    return new HelloServiceBlockingStub(channel);
+  }
+
+  /**
+   * Creates a new ListenableFuture-style stub that supports unary calls on the service
+   */
+  public static HelloServiceFutureStub newFutureStub(
+      io.grpc.Channel channel) {
+    return new HelloServiceFutureStub(channel);
+  }
+
+  /**
+   */
+  public static abstract class HelloServiceImplBase implements io.grpc.BindableService {
+
+    /**
+     */
+    public void sayHello(com.springboot.rpc.nameresovler.HelloMessage request,
+        io.grpc.stub.StreamObserver<com.springboot.rpc.nameresovler.HelloResponse> responseObserver) {
+      asyncUnimplementedUnaryCall(getSayHelloMethod(), responseObserver);
+    }
+
+    @java.lang.Override public final io.grpc.ServerServiceDefinition bindService() {
+      return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
+          .addMethod(
+            getSayHelloMethod(),
+            asyncUnaryCall(
+              new MethodHandlers<
+                com.springboot.rpc.nameresovler.HelloMessage,
+                com.springboot.rpc.nameresovler.HelloResponse>(
+                  this, METHODID_SAY_HELLO)))
+          .build();
+    }
+  }
+
+  /**
+   */
+  public static final class HelloServiceStub extends io.grpc.stub.AbstractStub<HelloServiceStub> {
+    private HelloServiceStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private HelloServiceStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected HelloServiceStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new HelloServiceStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public void sayHello(com.springboot.rpc.nameresovler.HelloMessage request,
+        io.grpc.stub.StreamObserver<com.springboot.rpc.nameresovler.HelloResponse> responseObserver) {
+      asyncUnaryCall(
+          getChannel().newCall(getSayHelloMethod(), getCallOptions()), request, responseObserver);
+    }
+  }
+
+  /**
+   */
+  public static final class HelloServiceBlockingStub extends io.grpc.stub.AbstractStub<HelloServiceBlockingStub> {
+    private HelloServiceBlockingStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private HelloServiceBlockingStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected HelloServiceBlockingStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new HelloServiceBlockingStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public com.springboot.rpc.nameresovler.HelloResponse sayHello(com.springboot.rpc.nameresovler.HelloMessage request) {
+      return blockingUnaryCall(
+          getChannel(), getSayHelloMethod(), getCallOptions(), request);
+    }
+  }
+
+  /**
+   */
+  public static final class HelloServiceFutureStub extends io.grpc.stub.AbstractStub<HelloServiceFutureStub> {
+    private HelloServiceFutureStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private HelloServiceFutureStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected HelloServiceFutureStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new HelloServiceFutureStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public com.google.common.util.concurrent.ListenableFuture<com.springboot.rpc.nameresovler.HelloResponse> sayHello(
+        com.springboot.rpc.nameresovler.HelloMessage request) {
+      return futureUnaryCall(
+          getChannel().newCall(getSayHelloMethod(), getCallOptions()), request);
+    }
+  }
+
+  private static final int METHODID_SAY_HELLO = 0;
+
+  private static final class MethodHandlers<Req, Resp> implements
+      io.grpc.stub.ServerCalls.UnaryMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
+    private final HelloServiceImplBase serviceImpl;
+    private final int methodId;
+
+    MethodHandlers(HelloServiceImplBase serviceImpl, int methodId) {
+      this.serviceImpl = serviceImpl;
+      this.methodId = methodId;
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public void invoke(Req request, io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        case METHODID_SAY_HELLO:
+          serviceImpl.sayHello((com.springboot.rpc.nameresovler.HelloMessage) request,
+              (io.grpc.stub.StreamObserver<com.springboot.rpc.nameresovler.HelloResponse>) responseObserver);
+          break;
+        default:
+          throw new AssertionError();
+      }
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public io.grpc.stub.StreamObserver<Req> invoke(
+        io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        default:
+          throw new AssertionError();
+      }
+    }
+  }
+
+  private static abstract class HelloServiceBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoFileDescriptorSupplier, io.grpc.protobuf.ProtoServiceDescriptorSupplier {
+    HelloServiceBaseDescriptorSupplier() {}
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.FileDescriptor getFileDescriptor() {
+      return com.springboot.rpc.nameresovler.HelloWorldGrpc.getDescriptor();
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.ServiceDescriptor getServiceDescriptor() {
+      return getFileDescriptor().findServiceByName("HelloService");
+    }
+  }
+
+  private static final class HelloServiceFileDescriptorSupplier
+      extends HelloServiceBaseDescriptorSupplier {
+    HelloServiceFileDescriptorSupplier() {}
+  }
+
+  private static final class HelloServiceMethodDescriptorSupplier
+      extends HelloServiceBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoMethodDescriptorSupplier {
+    private final String methodName;
+
+    HelloServiceMethodDescriptorSupplier(String methodName) {
+      this.methodName = methodName;
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.MethodDescriptor getMethodDescriptor() {
+      return getServiceDescriptor().findMethodByName(methodName);
+    }
+  }
+
+  private static volatile io.grpc.ServiceDescriptor serviceDescriptor;
+
+  public static io.grpc.ServiceDescriptor getServiceDescriptor() {
+    io.grpc.ServiceDescriptor result = serviceDescriptor;
+    if (result == null) {
+      synchronized (HelloServiceGrpc.class) {
+        result = serviceDescriptor;
+        if (result == null) {
+          serviceDescriptor = result = io.grpc.ServiceDescriptor.newBuilder(SERVICE_NAME)
+              .setSchemaDescriptor(new HelloServiceFileDescriptorSupplier())
+              .addMethod(getSayHelloMethod())
+              .build();
+        }
+      }
+    }
+    return result;
+  }
+}
